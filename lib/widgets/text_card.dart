959970ed09e975017064b575364a824fcc30d717
Tyler Wilcox <tyler@tywilly.com>
2025-05-04 19:01:37 -0400

    Started Dashboard page

---
+++ lib/widgets/text_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TextCard extends StatefulWidget{
+  const TextCard({super.key, required this.cardTitle});
+
+  final String cardTitle;
+
+  @override
+  State<TextCard> createState() => _TextCardState();
+}
+
+class _TextCardState extends State<TextCard>{
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      child: Column( 
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget> [
+          Text(widget.cardTitle),
+          SizedBox(width: 100, height: 10),
+          Text("THE COOL TEXT")
+        ]
+      ),
+    );
+  }
+}
