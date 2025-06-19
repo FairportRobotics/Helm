b35ca5460dd14b6e1c005edf16eabc71f1d576a5
Tyler Wilcox <tyler@wilcoxflp.com>
2025-06-19 10:02:10 -0400

    Better NT subscription handling

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,15 +4,17 @@ import "package:nt4/nt4.dart";
 
 NT4Client ntClient = NT4Client(serverBaseAddress: '10.5.78.2', onConnect: () { print("NT4 Connected"); }, onDisconnect: () { print("NT4 Disconnected"); });
 
-List<NT4Subscription> nt4Subscriptions = [];
+Map<String, NT4Subscription> nt4Subscriptions = {};
 
 NT4Subscription subscribe(String topic){
 
-  NT4Subscription subscrip = ntClient.subscribe(topic, NT4SubscriptionOptions());
+  if (!nt4Subscriptions.containsKey(topic))
+  {
+    NT4Subscription subscrip = ntClient.subscribe(topic, NT4SubscriptionOptions());
+    nt4Subscriptions[topic] = subscrip;
+  }
 
-  nt4Subscriptions.add(subscrip);
-
-  return subscrip;
+  return nt4Subscriptions[topic]!;
 }
 
 List<NT4Topic> nt4Topics = [];
