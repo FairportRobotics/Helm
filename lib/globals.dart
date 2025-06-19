e38494104ba479a687b289c7e782372078b9c79a
Tyler Wilcox <tyler@wilcoxflp.com>
2025-06-18 23:06:53 -0400

    Network tables impl

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,6 +2,17 @@ library;
 
 import "package:nt4/nt4.dart";
 
+NT4Client ntClient = NT4Client(serverBaseAddress: '10.5.78.2', onConnect: () { print("NT4 Connected"); }, onDisconnect: () { print("NT4 Disconnected"); });
 
+List<NT4Subscription> nt4Subscriptions = [];
 
-NT4Client? ntClient;
+NT4Subscription subscribe(String topic){
+
+  NT4Subscription subscrip = ntClient.subscribe(topic, NT4SubscriptionOptions());
+
+  nt4Subscriptions.add(subscrip);
+
+  return subscrip;
+}
+
+List<NT4Topic> nt4Topics = [];
